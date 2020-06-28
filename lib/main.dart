bfb00b6452a10c49daebd6ed2d6ff43f76e484e8
Eugenio Barros <eugenio.barros.jr@gmail.com>
2020-06-28 15:33:57 -0300

    feature: add TransferList, TrasferItem & Trasnfer

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,44 @@ class MyApp extends StatelessWidget {
           child: Icon(Icons.add),
           onPressed: () {},
         ),
+        body: TransferList(),
       ),
     );
   }
+}
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferItem(Transfer(2000.00, '3231312')),
+        TransferItem(Transfer(2012.00, '32312')),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final String accountNumber;
+
+  Transfer(this.value, this.accountNumber);
 }
